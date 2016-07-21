28159239af92de3fe58dd30c2f17ea4268dd2962
tankerkiller125 <tankerkiller125@gmail.com>
2016-07-21 07:19:26 -0400

    Needed a real Reason for the Disconnect and not just a string.

--- src/main/java/ml/rhodes/discordbot/commands/admin/Disconnect.java
+++ src/main/java/ml/rhodes/discordbot/commands/admin/Disconnect.java
@@ -7,11 +7,12 @@ import java.util.Objects;
 
 import static ml.rhodes.discordbot.Core.apiKey;
 import static ml.rhodes.discordbot.Core.getClient;
+import static sx.blah.discord.handle.impl.events.DiscordDisconnectedEvent.Reason.LOGGED_OUT;
 
 public class Disconnect implements IListener<DiscordDisconnectedEvent> {
     public void handle(DiscordDisconnectedEvent event) {
         System.out.println("Disconnected : " + event.getReason());
-        if (!Objects.equals("LOGGED_OUT", event.getReason())) {
+        if (!Objects.equals(LOGGED_OUT, event.getReason())) {
             try {
                 getClient(apiKey);
             } catch (Exception e) {
