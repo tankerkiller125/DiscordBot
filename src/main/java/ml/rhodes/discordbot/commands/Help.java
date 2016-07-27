32df3b5e90ee5ca76f5704f2ff7f50ee3363cd98
tankerkiller125 <tankerkiller125@gmail.com>
2016-07-27 18:43:12 -0400

    Help Owner now changes based on configured owner ID

--- src/main/java/ml/rhodes/discordbot/commands/Help.java
+++ src/main/java/ml/rhodes/discordbot/commands/Help.java
@@ -4,20 +4,24 @@ import sx.blah.discord.api.IListener;
 import sx.blah.discord.handle.impl.events.MessageReceivedEvent;
 import sx.blah.discord.handle.obj.IPrivateChannel;
 
-import static ml.rhodes.discordbot.Core.discordClient;
-import static ml.rhodes.discordbot.Core.version;
+import static ml.rhodes.discordbot.Core.*;
 
 public class Help implements IListener<MessageReceivedEvent> {
     public void handle(MessageReceivedEvent event) {
         Boolean command = event.getMessage().getContent().startsWith("-help");
         String content =
-                "**Owner: tankerkiller125**\n" +
+                "**Owner: " + discordClient.getUserByID(config.getString("discord.owner")).getName() + "**\n" +
                         "**Version: " + version + "**\n" +
                         "__**Commands:**__\n" +
                         "    **-help** : Gives you this menu\n" +
                         "    **-weather {city},{country code (optional)}** : Current weather for that city\n" +
                         "    **-youtube {id}** : Get youtube stats\n" +
                         "    **-repostats {owner}/{repository}** : Get GitHub repository stats\n" +
+                        "\n" +
+                        "__**Helpers:**__\n" +
+                        "    **{user}/{repo}** : Basic info and link to Github Repo\n" +
+                        "    **{user}/{repo}#{issue}** : Link to GitHub Issue/Pull Request\n" +
+                        "    **{user}/{repo}@{commit}** : Link to GitHub Commit\n" +
                         "\n" +
                         "__**Admin Only:**__\n" +
                         "    **-changegame {text for game}** : Change the game the bots playing\n" +
